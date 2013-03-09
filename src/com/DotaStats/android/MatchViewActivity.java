17e24d55948bafb02778c1a7074056fd4b6d660f
Daniel <daniel@daniel-sandbox.(none)>
2013-03-09 01:56:33 -0500

    -Last commit before match view interface is added.

--- src/com/DotaStats/android/MatchViewActivity.java
+++ src/com/DotaStats/android/MatchViewActivity.java
@@ -1,14 +1,19 @@
 package com.DotaStats.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MatchViewActivity extends Activity {
+	
+	String matchID;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.match_view_layout);
+		Intent intent = getIntent();
+		this.matchID = intent.getStringExtra("matchID");
 	}
 
 }
