bd270dd0d0c081969dcd24b0c2f82b37232a78ba
Daniel <daniel@daniel-sandbox.(none)>
2013-03-09 22:01:24 -0500

    -Added an interface to the individual stats page.

--- src/com/DotaStats/android/IndividualStatsActivity.java
+++ src/com/DotaStats/android/IndividualStatsActivity.java
@@ -1,7 +1,5 @@
 package com.DotaStats.android;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,16 +7,13 @@ import android.os.Bundle;
 public class IndividualStatsActivity extends Activity {
 	
 	private JSONParser parser;
-	private List<Player> playerList;
-	private int currentIndex = 0;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.individual_stats_layout);
 		Intent intent = getIntent();
 		this.parser = new JSONParser(intent.getStringExtra("matchID"));
-		this.playerList = parser.getPlayerList();
+		parser.getPlayerList();
 		setWidgets();
 		populateData(0);
 	}
@@ -28,7 +23,7 @@ public class IndividualStatsActivity extends Activity {
 	}
 	
 	private void populateData(int index) {
-		Player currentPlayer = playerList.get(index);
+		
 	}
 
 }
--- src/com/DotaStats/android/MatchViewActivity.java
+++ src/com/DotaStats/android/MatchViewActivity.java
@@ -1,17 +1,9 @@
 package com.DotaStats.android;
 
-import java.io.IOException;
 import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
