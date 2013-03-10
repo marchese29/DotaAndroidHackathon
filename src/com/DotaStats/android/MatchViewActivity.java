c2755c601aefd3b380a5507b35c6ea87c3fd05f8
Daniel <daniel@daniel-sandbox.(none)>
2013-03-10 04:55:12 -0400

    -App now downloads hero images and displays them

--- src/com/DotaStats/android/MatchViewActivity.java
+++ src/com/DotaStats/android/MatchViewActivity.java
@@ -1,12 +1,23 @@
 package com.DotaStats.android;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -14,9 +25,8 @@ public class MatchViewActivity extends Activity {
 
 	// Widgets
 	private Button returnHomeButton;
-	private Button individualStatsButton;
 	private TableRow[] table;
-	
+
 	private String matchID;
 
 	private JSONParser parser;
@@ -32,7 +42,7 @@ public class MatchViewActivity extends Activity {
 		 * TODO: Create all of the JSON objects using the JSONParser class.
 		 * Also, make sure the table has loaded correctly.
 		 */
-		
+
 		setWidgets();
 		setWidgets2();
 		this.parser = new JSONParser(matchID);
@@ -65,7 +75,7 @@ public class MatchViewActivity extends Activity {
 		this.table[2] = (TableRow) findViewById(R.id.trPlayer3Row);
 		this.table[3] = (TableRow) findViewById(R.id.trPlayer4Row);
 		this.table[4] = (TableRow) findViewById(R.id.trPlayer5Row);
-		
+
 		this.table[5] = (TableRow) findViewById(R.id.trPlayer6Row);
 		this.table[6] = (TableRow) findViewById(R.id.trPlayer7Row);
 		this.table[7] = (TableRow) findViewById(R.id.trPlayer8Row);
@@ -79,9 +89,16 @@ public class MatchViewActivity extends Activity {
 			List<Integer> playerStats = currentPlayer.getIntList();
 			TextView playerName = (TextView) this.table[i].getVirtualChildAt(0);
 			playerName.setText(currentPlayer.name);
-			// TextView heroName = (TextView)
-			// this.table[i].getVirtualChildAt(1);
-			// heroName.setText(currentPlayer.HeroName);
+
+			ImageView heroPicture = (ImageView) this.table[i]
+					.getVirtualChildAt(1);
+			try {
+				heroPicture.setImageBitmap(new GetWebImage().execute(currentPlayer.HeroName).get());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
 
 			for (int j = 2; j < 9; j++) {
 				TextView view = (TextView) this.table[i].getVirtualChildAt(j);
@@ -94,6 +111,33 @@ public class MatchViewActivity extends Activity {
 	protected void onDestroy() {
 		super.onDestroy();
 		this.finish();
+	}
+
+}
+
+class GetWebImage extends AsyncTask<String, Void, Bitmap> {
+
+	@Override
+	protected Bitmap doInBackground(String... heroName) {
+		URL url;
+		Bitmap result = null;
+		try {
+			url = new URL(
+					"http://dotawebapihackathon.apphb.com/content/heroportraits/"
+							+ heroName[0].replace(' ', '_') + "_full.png");
+			URLConnection connection = url.openConnection();
+			HttpURLConnection HCon = (HttpURLConnection) connection;
+			InputStream ins = ((URLConnection) HCon).getInputStream();
+			result = BitmapFactory.decodeStream(ins);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
 	}
 
 }
